2c031c87c075f0d08d82c62320e8c1828725f3b8
Become-rpg <fabian.cedo@gmail.com>
2026-08-29 18:34:17 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "become-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.add("./"))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
